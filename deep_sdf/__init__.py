f06bdbe7ec3fc49d8b1c73a0682b2e0fbe976ad7
JinShuo003 <710083000@qq.com>
2023-03-09 15:42:14 +0800

    修改项目结构

--- deep_sdf/__init__.py
+++ deep_sdf/__init__.py
@@ -2,7 +2,5 @@
 # Copyright 2004-present Facebook. All Rights Reserved.
 
 from deep_sdf.data import *
-from deep_sdf.mesh import *
-from deep_sdf.metrics.chamfer import *
 from deep_sdf.utils import *
 from deep_sdf.workspace import *
